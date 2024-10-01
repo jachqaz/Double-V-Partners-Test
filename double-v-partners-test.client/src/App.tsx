328c508a4360ded1cbbdef11befeec3fd99f6440
Johan Castaño <jach_1997@hotmail.com>
2024-10-01 01:02:27 -0500

    Add Ionic on React Client
    Signed-off-by: Johan Castaño <jach_1997@hotmail.com>

--- double-v-partners-test.client/src/App.tsx
+++ double-v-partners-test.client/src/App.tsx
@@ -1,5 +1,20 @@
 import {useEffect, useState} from 'react';
 import './App.css';
+import '@ionic/react/css/core.css';
+import {IonApp, IonContent, setupIonicReact} from '@ionic/react';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+setupIonicReact();
 
 interface Forecast {
     date: string;
@@ -41,11 +56,15 @@ function App() {
         </table>;
 
     return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+        <IonApp>
+            <IonContent fullscreen>
+                <div>
+                    <h1 id="tableLabel">Weather forecast</h1>
+                    <p>This component demonstrates fetching data from the server.</p>
+                    {contents}
+                </div>
+            </IonContent>
+        </IonApp>
     );
 
     async function populateWeatherData() {
